63d1d99c2645cf0e51f871d00126f17b7912174a
Reina Wallace <reiwallace@yahoo.com>
2025-03-12 10:37:43 +0000

    began implemenation of background image

--- bullet_hell/src/main/java/pp2/GUI/GameFrame.java
+++ bullet_hell/src/main/java/pp2/GUI/GameFrame.java
@@ -1,22 +1,27 @@
 package pp2.GUI;
 
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import pp2.ImageHandler;
 
 public class GameFrame {
     private final Rectangle gameFrame = new Rectangle();
-    
     private final int[] gameFrameSize = {24, 28}; // Size of gameFrame in tiles
-    private final ImageHandler backgroundImage = new ImageHandler(); // Background image
+    private final String bgPath = "images/gameBGTEST.jpg";
+
+    private ImageView backgroundImage; // Background image
 
     // Set up default game frame with its size
     public GameFrame(double sceneHeight, double stageWidth) {
         gameFrame.setWidth((stageWidth/40)*gameFrameSize[0]);
         gameFrame.setHeight((sceneHeight/30)*gameFrameSize[1]);
         gameFrame.setFill(Color.BLACK);
+        backgroundImage.setImage(ImageHandler.loadImage("images/enemy.png"));
     }
 
     // Setters and getters
     public Rectangle getGameFrame() { return gameFrame; }
+
+    public void setBackgroundImage(String imagePath) { backgroundImage.setImage(ImageHandler.loadImage(imagePath)); }
 }
