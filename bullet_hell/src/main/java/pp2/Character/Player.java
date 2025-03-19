949f35c37869c25e0f58b60f32ffa18b336b718e
Martin Adegbola Traore <martin.a.traore@outlook.com>
2025-03-19 18:01:28 +0000

    Player class implemented. Just need to test with the game frame

--- bullet_hell/src/main/java/pp2/Character/Player.java
+++ bullet_hell/src/main/java/pp2/Character/Player.java
@@ -1,16 +1,13 @@
 package pp2.Character;
 
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import pp2.Entity.Entity;
 
 public class Player extends Entity{
 
-    private double x_pos = 300; // Horizontal position
-    private double y_pos = 250; // Vertical position
-    private int widthHeight = 50; // Width and Height
-    private int speed; // Player movement speed
+    private final int speed; // Player movement speed
+    private int x_pos, y_pos; // Player position
 
 
     // Player character constructor
@@ -18,28 +15,53 @@ public class Player extends Entity{
         super(imagePath, health); // Call Entity constructor
         this.speed = speed;
         this.gamePane = gamePane; // Assign game pane
+        this.x_pos = (int) entityImage.getLayoutX();
+        this.y_pos = (int) entityImage.getLayoutY();
+
+        // Set player characteristics
+        entityImage.setId("player");
+        entityImage.setFitWidth(50);
+        entityImage.setFitHeight(50);
+        entityImage.setFocusTraversable(true);
+        entityImage.setPickOnBounds(true);
+        entityImage.setPreserveRatio(true);
+        entityImage.setSmooth(true);
+
+        // Add the player to the main pane
+        gamePane.getChildren().add(getImage());
     }
 
     // Moves the player based on input direction.
     @Override
     public void move(KeyCode key) {
-        double x = entityImage.getLayoutX();
-        double y = entityImage.getLayoutY();
+        double newX = x_pos;
+        double newY = y_pos;
 
         switch (key) {
             case LEFT:
-                setPosition((int) (x - speed), (int) y); // Move left
+                newX -= speed; // Move left
                 break;
             case RIGHT:
-                setPosition((int) (x + speed), (int) y); // Move right
+                newX += speed; // Move right
                 break;
             case UP:
-                setPosition((int) x, (int) (y - speed)); // Move up
+                newY -= speed; // Move up
                 break;
             case DOWN:
-                setPosition((int) x, (int) (y + speed)); // Move down
+                newY += speed; // Move down
                 break;
         }
+
+        // Ensure the player does not go out of bounds
+        if (newX >= 0 && newX + entityImage.getFitWidth() <= gamePane.getWidth()) {
+            x_pos = (int) newX;
+        }
+        if (newY >= 0 && newY + entityImage.getFitHeight() <= gamePane.getHeight()) {
+            y_pos = (int) newY;
+        }
+
+        // Update position
+        setPosition(x_pos, y_pos);
     }
 
     // Reduces the player's health when hit.
@@ -59,5 +81,25 @@ public class Player extends Entity{
     public void destroy() {
         System.out.println("Player destroyed!");
         gamePane.getChildren().removeAll(entityImage, getHitbox());
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getX_pos() {
+        return x_pos;
+    }
+
+    public void setX_pos(int x_pos) {
+        this.x_pos = x_pos;
+    }
+
+    public int getY_pos() {
+        return y_pos;
+    }
+
+    public void setY_pos(int y_pos) {
+        this.y_pos = y_pos;
     }
 }
