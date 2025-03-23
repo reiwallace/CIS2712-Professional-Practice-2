7784d91998815c7b1a47b717352c4dd8ede3fb9a
Reina <reiwallace@yahoo.com>
2025-03-23 17:44:01 +0000

    added player and background to resources file

--- bullet_hell/src/main/java/pp2/Resources/BackgroundImages.java
+++ bullet_hell/src/main/java/pp2/Resources/BackgroundImages.java
@@ -1,5 +1,6 @@
 package pp2.Resources;
 
 public class BackgroundImages {
-
+    private String player = "https://i.ibb.co/LhYpPskV/player.png";
+    private String background = "https://i.ibb.co/YBZzsddp/game-BGTEST.jpg";
 }
