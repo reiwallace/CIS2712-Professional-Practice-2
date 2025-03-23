ca244bb0380d385714ea8b8b293d3d975a693338
Reina <reiwallace@yahoo.com>
2025-03-23 17:59:42 +0000

    Deleted redundant css file and began work on player statistics panel

---
+++ bullet_hell/src/main/java/pp2/GUI/PlayerStatisticsPanel.java
@@ -0,0 +1,17 @@
+package pp2.GUI;
+
+import pp2.Score;
+
+public class PlayerStatisticsPanel {
+    private MainWindow mainWindow;
+    private Score score = new Score();
+    
+    /** Initialise Player stats panel
+     * @param mainWindow - Window to attach panel to
+     */
+    public PlayerStatisticsPanel(MainWindow mainWindow) {
+        this.mainWindow = mainWindow;
+
+
+    }
+}
